176ae97164636af38c8f237c08373520b8ba9ba4
avigmati <avigmati@gmail.com>
2017-06-06 19:24:05 +0700

    add model: AnotherModel

--- testapp/models.py
+++ testapp/models.py
@@ -3,3 +3,7 @@ from django.db import models
 
 class TestModel(models.Model):
     name = models.CharField(max_length=100)
+
+
+class AnotherModel(models.Model):
+    title = models.CharField(max_length=100)
